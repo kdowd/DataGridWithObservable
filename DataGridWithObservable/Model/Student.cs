17a4fb8f484a7b2766cb46ad6634337c8d9a2ac0
kdowd <kevin.dowd@gmail.com>
2024-05-19 22:52:22 +1200

    Add project files.

---
+++ DataGridWithObservable/Model/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridWithObservable.Model
+{
+    internal class Student
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; } = 0;
+        public bool isSoccerPlayer { get; set; } = true;
+
+
+        private Gender thegender;
+
+        public Gender TheGender
+        {
+            get { return thegender; }
+            set { thegender = value; }
+        }
+
+
+        public static ObservableCollection<Student> GetStudents()
+        {
+            var allStudents = new ObservableCollection<Student>();
+            allStudents.Add(new Student() { Name = "John", Age = 45, isSoccerPlayer = true, TheGender = Gender.Male });
+            allStudents.Add(new Student() { Name = "Jane", Age = 22, isSoccerPlayer = true, TheGender = Gender.Female });
+
+            return allStudents;
+
+        }
+
+
+        public enum Gender
+        {
+            Male, Female
+        }
+    }
+}
